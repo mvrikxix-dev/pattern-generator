6f61e599905db5d45d66b9ea9c17bf19f26ba484
mvrikxix-dev <mvrikxix-dev@gmail.com>
2020-04-30 02:13:14 +0530

    Workspace created

--- app.js
+++ app.js
@@ -2,36 +2,40 @@ var landscape = document.querySelector('.landscape');
 var portrait = document.querySelector('.portrait');
 var square = document.querySelector('.square');
 var menu = document.getElementById('menu');
-var menu_cancel = document.querySelector('.cancel');
 var canvas_width = document.getElementById('width-input');
 var canvas_height = document.getElementById('height-input');
 var filename = document.getElementById('filename').textContent;
-var artboard = document.getElementById('artboard-input').value;
+var workspace = document.getElementById('workspace');
+var canvas = document.querySelector('.canvas');
 
 
-menu_cancel.addEventListener('click', function() {
-   menu.style.display="none"; 
+workspace.style.display = "none";
+
+document.querySelector('.cancel-0').addEventListener('click', function () {
+    menu.style.display = "none";
 });
 
+document.querySelector('.cancel-1').addEventListener('click', function () {
+    workspace.style.display = "none";
+});
 
-landscape.addEventListener('click', function() {
+landscape.addEventListener('click', function () {
     landscape.classList.add('orient-active');
     portrait.classList.remove('orient-active');
-        square.classList.remove('orient-active');
+    square.classList.remove('orient-active');
     canvas_width.value = '1080';
     canvas_height.value = '566';
 });
 
-portrait.addEventListener('click', function() {
+portrait.addEventListener('click', function () {
     portrait.classList.add('orient-active');
     landscape.classList.remove('orient-active');
-        square.classList.remove('orient-active');
-
+    square.classList.remove('orient-active');
     canvas_width.value = '1080';
     canvas_height.value = '1350';
 });
 
-square.addEventListener('click', function() {
+square.addEventListener('click', function () {
     square.classList.add('orient-active');
     landscape.classList.remove('orient-active');
     portrait.classList.remove('orient-active');
@@ -45,9 +49,14 @@ function isNumberCheck(evt) {
     return charCode;
 };
 
-document.getElementById('create').addEventListener('click', function() {
-    filename = document.getElementById('filename').textContent; 
-    menu.style.display="none";
-    artboard = document.getElementById('artboard-input').value;
+document.querySelector('.create').addEventListener('click', function () {
+    filename = document.getElementById('filename').textContent;
+    menu.style.display = "none";
+    workspace.style.display = "block";
+    canvas.style.width = canvas_width.value / 8 + "px";
+    canvas.style.height = canvas_height.value / 8 + "px";
 });
 
+document.querySelector('.next-0').addEventListener('click', function () {
+    workspace.style.display = "none";
+});
