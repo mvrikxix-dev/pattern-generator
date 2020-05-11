f1ad68e9604fed583943aebce886864797ce1758
mvrikxix-dev <mvrikxix-dev@gmail.com>
2020-05-11 14:16:22 +0530

    Custom Color panel added; Menu fonts changed to png, icomoon cdn removed

--- js/workspace.js
+++ js/workspace.js
@@ -2,6 +2,9 @@ var bg_color = document.querySelector('.color-active');
 var workspace = document.getElementById('workspace');
 var canvas = document.querySelector('.canvas');
 var color_container = document.querySelector('.color-container');
+var custom_color = document.querySelector(".custom-color");
+
+custom_color.style.display = "none";
 
 color_container.addEventListener('click', function(e) {
     if(e.target.className === 'color'){
@@ -11,8 +14,23 @@ color_container.addEventListener('click', function(e) {
     }
 })
 
-
-
 document.querySelector('.next-0').addEventListener('click', function () {
     workspace.style.display = "none";
+});
+
+document.querySelector('.add-color').addEventListener('click', function() {
+    custom_color.style.display = "block";
+});
+
+document.querySelector('.cancel-2').addEventListener('click', function() {
+    custom_color.style.display = "none";
+});
+
+document.getElementById('custom-add').addEventListener('click', function() {
+    if(document.getElementById('custom').value === '') {
+        alert('The Custom Color box is empty, kindly add a value');
+    }else {
+        canvas.style.backgroundColor = document.getElementById('custom').value;
+        custom_color.style.display = "none";
+    } 
 });
